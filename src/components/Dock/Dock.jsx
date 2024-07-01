1eea5689369db46ee4c2b7ff91509f400de596f2
rjohnsondev1 <rjohnsondev1@gmail.com>
2024-06-30 23:17:25 -0400

    well

--- src/components/Dock/Dock.jsx
+++ src/components/Dock/Dock.jsx
@@ -1,13 +1,10 @@
-import React, { Component} from 'react'
-import './Dock.scss'
-//import About from '../About/About'
-import Finder from '../Finder/Finder'
-import Email from '../Email/email'
-import Launchpad1 from '../Launchpad1/Launchpad1'
-import Projects from '../Projects/Projects'
-import { finder1, launch, email, folder, safari, settings, Trashfull, bookfront } from '../../assets'
-
-
+import React, { Component } from 'react';
+import './Dock.scss';
+import Finder from '../Finder/Finder';
+import Email from '../Email/email';
+import Launchpad1 from '../Launchpad1/Launchpad1';
+import Projects from '../Projects/Projects';
+import { finder1, launch, email, projects, safari, settings, Trashfull, bookfront } from '../../assets';
 
 class Dock extends Component {
     constructor(props) {
@@ -17,126 +14,108 @@ class Dock extends Component {
             showBookFront: false,
             showFinder: false,
             showLaunchpad1: false,
-            showProjects: false
-
-            
+            showProjects: false,
         };
         this.bookfrontRef = React.createRef();
-
-        this.finderListener = () => {
-            this.setState( prevState => ({showFinder: !prevState.showFinder}) );
-            console.log('finder clicked')
-        }
-
-
-          
-
-        this.emailListener  = () => {
-            this.setState( prevState => ({showEmail: !prevState.showEmail}) );
-            console.log('email clicked')
-        }
-
-        this.ProjectsListener = () => {
-            this.setState( prevState => ({showProjects: !prevState.showProjects}) );
-            console.log('projects clicked')
-        }
-
-        this.safariListener = () => {
-            this.setState( prevState => ({showBookFront: !prevState.showBookFront}) );
-            console.log('safari clicked')
-        } 
-
-        this.launchpad1Listener = () => {
-            this.setState( prevState => ({showLaunchpad1: !prevState.showLaunchpad1}) );
-            console.log('launchpad clicked')
-        }
     }
 
     componentDidMount() {
         document.addEventListener('mousedown', this.handleClickOutside);
     }
-  
+
     componentWillUnmount() {
         document.removeEventListener('mousedown', this.handleClickOutside);
     }
-  
+
     handleClickOutside = (event) => {
         if (this.bookfrontRef.current && !this.bookfrontRef.current.contains(event.target)) {
             this.setState({ showBookFront: false });
         }
     }
-  
-      closeListener = () => {
-          this.setState({isFinderOpen: false});
-          this.setState({isProjectsOpen: false});
-          this.setState({isLaunchpad1Open: false});
-          console.log('finder closed')
-      }
 
-render() {
+    finderListener = () => {
+        this.setState(prevState => ({ showFinder: !prevState.showFinder }));
+        console.log('finder clicked');
+    }
 
-   
-  return (
-   
-    <section className='dock-container'>
-    <div className='dock-item'>
-        <div className='tool-tip'> 
-            <p>Skills</p>
-        </div>
-        <img src={finder1} onClick={this.finderListener} alt="finder" id='finder' className='image-wrapper'  />
-    </div>
-    <div className='dock-item'>
-        <div className='tool-tip'>
-            <p>Tech</p>
-        </div>
-        <img src={launch} onClick={this.launchpad1Listener} alt="launch" id='launch' className='image-wrapper' />
-    </div>
-    <div className='dock-item'>
-        <div className='tool-tip'>
-            <p>Email</p>
-        </div>
-        <img src={email} onClick={this.emailListener} alt="email" id='email' className='image-wrapper' />
-    </div>
-    <div className='dock-item'>
-        <div className='tool-tip'>
-            <p>Projects</p>
-        </div>
-        <img src={folder} onClick={this.projectsListener} alt="folder" id='folder' className='image-wrapper' />
-    </div>
-    <div className='dock-item' ref={this.ref}>
-        <div className='tool-tip'>
-            <p>About</p>
-        </div>
-        <img src={safari} onClick={this.safariListener } alt="safari" id='safari' className='image-wrapper' ref={this.ref} />
-        
-    </div>
-    <div className='dock-item'>
-    <div className='tool-tip'>
-     <p>Settings</p>
-    </div>
-        <img src={settings} alt="settings" id='settings' className='image-wrapper' />
-    </div>
-    <div className='dock-item'>
-        <div className='tool-tip'>
-            <p>Trash</p>
-    </div>
-        <img src={Trashfull} alt="trash" />
-    </div>
+    emailListener = () => {
+        this.setState(prevState => ({ showEmail: !prevState.showEmail }));
+        console.log('email clicked');
+    }
 
-    {this.state.showFinder && <Finder/>}
-    {this.state.showLaunchpad1 && <Launchpad1/>}
-    {this.state.showBookFront && (
-            <img className="bookfront-display" alt="Bookfront displayed" src={bookfront} ref={this.bookfrontRef} />
-          )}
-    {this.state.showEmail && <Email/>}
+    projectsListener = () => {
+        this.setState(prevState => ({ showProjects: !prevState.showProjects }));
+        console.log('projects clicked');
+    }
 
-       
-    </section >
-   
-    
-  )
-  
+    safariListener = () => {
+        this.setState(prevState => ({ showBookFront: !prevState.showBookFront }));
+        console.log('safari clicked');
+    }
+
+    launchpad1Listener = () => {
+        this.setState(prevState => ({ showLaunchpad1: !prevState.showLaunchpad1 }));
+        console.log('launchpad clicked');
+    }
+
+    render() {
+        const { showFinder, showLaunchpad1, showProjects, showBookFront, showEmail } = this.state;
+
+        return (
+            <section className='dock-container'>
+                <div className='dock-item'>
+                    <div className='tool-tip'>
+                        <p>Skills</p>
+                    </div>
+                    <img src={finder1} onClick={this.finderListener} alt="finder" id='finder' className='image-wrapper' />
+                </div>
+                <div className='dock-item'>
+                    <div className='tool-tip'>
+                        <p>Tech</p>
+                    </div>
+                    <img src={launch} onClick={this.launchpad1Listener} alt="launch" id='launch' className='image-wrapper' />
+                </div>
+                <div className='dock-item'>
+                    <div className='tool-tip'>
+                        <p>Email</p>
+                    </div>
+                    <img src={email} onClick={this.emailListener} alt="email" id='email' className='image-wrapper' />
+                </div>
+                <div className='dock-item'>
+                    <div className='tool-tip'>
+                        <p>Projects</p>
+                    </div>
+                    <img src={projects} onClick={this.projectsListener} alt="projects" id='projects' className='image-wrapper' />
+                </div>
+                <div className='dock-item' ref={this.bookfrontRef}>
+                    <div className='tool-tip'>
+                        <p>About</p>
+                    </div>
+                    <img src={safari} onClick={this.safariListener} alt="safari" id='safari' className='image-wrapper' />
+                </div>
+                <div className='dock-item'>
+                    <div className='tool-tip'>
+                        <p>Settings</p>
+                    </div>
+                    <img src={settings} alt="settings" id='settings' className='image-wrapper' />
+                </div>
+                <div className='dock-item'>
+                    <div className='tool-tip'>
+                        <p>Trash</p>
+                    </div>
+                    <img src={Trashfull} alt="trash" />
+                </div>
+
+                {showFinder && <Finder />}
+                {showLaunchpad1 && <Launchpad1 />}
+                {showProjects && <Projects />}
+                {showBookFront && (
+                    <img className="bookfront-display" alt="Bookfront displayed" src={bookfront} ref={this.bookfrontRef} />
+                )}
+                {showEmail && <Email />}
+            </section>
+        );
+    }
 }
-}
 
-export default Dock
+export default Dock;
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,233 +1,155 @@
-import React, { Component } from 'react'
-import './Projects.scss'
-import { java, javascript, nodejs, react, threed, skills, Recent, vite1, threejs, html, css, aws, mysql, nextjs, postgresql, sass  } from '../../assets/Finder';
-
-
+import React, { Component } from 'react';
+import './Projects.scss';
+import { java, javascript, nodejs, react, threed, skills, Recent, vite1, threejs, html, css, aws, mysql, nextjs, postgresql, sass,  } from '../../assets/Finder';
+import { jestelleweb1 } from '../../assets';
 class Projects extends Component {
     constructor(props) {
         super(props);
-        this.state = {isProjectsOpen: true };
+        this.state = { isProjectsOpen: true };
         this.ref = React.createRef();
-        
     }
-
 
     componentDidMount() {
-      document.addEventListener('mousedown', this.handleClickOutside);
-  }
+        document.addEventListener('mousedown', this.handleClickOutside);
+    }
 
-  componentWillUnmount() {
-      document.removeEventListener('mousedown', this.handleClickOutside);
-  }
+    componentWillUnmount() {
+        document.removeEventListener('mousedown', this.handleClickOutside);
+    }
 
-  handleClickOutside = (event) => {
-      if (this.ref.current && !this.ref.current.contains(event.target)) {
-          this.setState({ isProjectsOpen: false });
-      }
-  }
+    handleClickOutside = (event) => {
+        if (this.ref.current && !this.ref.current.contains(event.target)) {
+            this.setState({ isProjectsOpen: false });
+        }
+    }
 
     closeListener = () => {
-        this.setState({isProjectsOpen: false});
-        console.log('Projects closed')
+        this.setState({ isProjectsOpen: false });
+        console.log('Projects closed');
     }
- 
 
-render () {
+    render() {
+        const { isProjectsOpen } = this.state;
 
-const {isProjectsOpen} = this.state;
-
-  return (
-     <>
-     {isProjectsOpen && (
-        <div className='finder-container' ref={this.ref}>
-                <div className='sidebarAndContentAreaWith'>
-                      <div className='sidebar'>
-                            <div className='toolbar'>
-                                  <div className='windowControls'>
-                                        <div className='window-button close' onClick={this.closeListener}></div> 
+        return (
+            <>
+                {isProjectsOpen && (
+                    <div className='finder-container' ref={this.ref}>
+                        <div className='sidebarAndContentAreaWith'>
+                            <div className='sidebar'>
+                                <div className='toolbar'>
+                                    <div className='windowControls'>
+                                        <div className='window-button close' onClick={this.closeListener}></div>
                                         <div className='winddow-button minimize' />
                                         <div className='zoom' />
-                                  </div>
-                            </div>
-                            <div className='sidebarList'>
-                                  <div className='sidebarGroup'>
+                                    </div>
+                                </div>
+                                <div className='sidebarList'>
+                                    <div className='sidebarGroup'>
                                         <div className='sectionHeader'>
-                                              <b className='section'>Favorites</b>
-                                              <div className='symbol'>􀆈</div>
+                                            <b className='section'>Favorites</b>
+                                            <div className='symbol'>􀆈</div>
                                         </div>
                                         <div className='component1'>
-                                              <div className='sidebarItemWrapper'>
-                                                    <div className='sidebarItem1'>
-                                                          <div className='leading'>
-                                                                <div className='symbol1' />
-                                                                <img className='skillsIcon' alt="" src={skills} />
-                                                                <div className='label'>Projects</div>
-                                                          </div>
-                                                          <div className='trailing' />
+                                            <div className='sidebarItemWrapper'>
+                                                <div className='sidebarItem1'>
+                                                    <div className='leading'>
+                                                        <div className='symbol1' />
+                                                        <img className='skillsIcon' alt="" src={skills} />
+                                                        <div className='label'>Projects</div>
                                                     </div>
-                                              </div>
-                                             
+                                                    <div className='trailing' />
+                                                </div>
+                                            </div>
                                         </div>
                                         <div className='sidebarItem1'>
-                                              <div className='leading'>
-                                                    <div className='symbol1'>􀣰</div>
-                                                    <div className='label'>Desktop</div>
-                                              </div>
-                                              <div className='trailing' />
+                                            <div className='leading'>
+                                                <div className='symbol1'>􀣰</div>
+                                                <div className='label'>Desktop</div>
+                                            </div>
+                                            <div className='trailing' />
                                         </div>
                                         <div className='sidebarItem1'>
-                                              <div className='leading'>
-                                                   
-                                                    <div className='symbol1'>
-                                                        <img alt='Recent'  src={Recent} />
-                                                    </div>
-                                                    <div className='label'>
-                                                          <span className='labelTxt'>
-                                                                <p className='recent'>Recent</p>
-                                                          </span>
-                                                    </div>
-                                              </div>
-                                              <div className='trailing' />
+                                            <div className='leading'>
+                                                <div className='symbol1'>
+                                                    <img alt='Recent' src={Recent} />
+                                                </div>
+                                                <div className='label'>
+                                                    <span className='labelTxt'>
+                                                        <p className='recent'>Recent</p>
+                                                    </span>
+                                                </div>
+                                            </div>
+                                            <div className='trailing' />
                                         </div>
-                                  </div>
-                                  <div className='sidebarItem1'>
+                                    </div>
+                                    <div className='sidebarItem1'>
                                         <div className='leading'>
-                                              <div className='symbol1'>􀁸</div>
-                                              <div className='label'>Downloads</div>
+                                            <div className='symbol1'>􀁸</div>
+                                            <div className='label'>Downloads</div>
                                         </div>
                                         <div className='trailing' />
-                                  </div>
-                                  <div className='sidebarGroup1'>
+                                    </div>
+                                    <div className='sidebarGroup1'>
                                         <div className='sectionHeader'>
-                                              <b className='section'>Locations</b>
-                                              <div className='symbol'>􀆈</div>
+                                            <b className='section'>Locations</b>
+                                            <div className='symbol'>􀆈</div>
                                         </div>
                                         <div className='sidebarItem4'>
-                                              <div className='leading'>
-                                                    <div className='symbol1'>􀤂</div>
-                                                    <div className='label'>Roberta’s Portfolio</div>
-                                              </div>
-                                              <div className='trailing' />
+                                            <div className='leading'>
+                                                <div className='symbol1'>􀤂</div>
+                                                <div className='label'>Roberta’s Portfolio</div>
+                                            </div>
+                                            <div className='trailing' />
                                         </div>
                                         <div className='sidebarItem5' />
-                                  </div>
+                                    </div>
+                                </div>
                             </div>
-                      </div>
-                      <div className='separator' />
-                      <div className='contentAreaAndToolbar'>
-                            <div className='contentArea'>
-                                  <div className='listItemPrimary'>
-                                        <div className='documentIconLabel'>
-                                              <div className='label5'> Vite</div>
-                                        </div>
-                                  </div>
-                                  <div className='listItemPrimary1'>
-                                        <div className='documentIconLabel'>
-                                              <div className='label5'> React</div>
-                                        </div>
-                                  </div>
-                                  <div className='listItemPrimary2'>
-                                        <div className='documentIconLabel'>
-                                              <div className='label5'> Sass</div>
-                                        </div>
-                                  </div>
-                                  <div className='listItemPrimary3'>
-                                        <div className='label5'> Node.js</div>
-                                  </div>
-                                  <div className='margins' />
-                                  <img className='nodejs1Icon' alt="" src={nodejs} />
-                                  <div className='listItemPrimary4'>
-                                        <div className='label5'> Three.js</div>
-                                  </div>
-                                  <img className='vitejs1Icon' alt="" src={vite1} />
-                                  <img className='brandThreejs1Icon' alt="" src={threejs} />
-                                  <img className='reactIcon' alt="" src={react} />
-                                  <div className='listItemPrimary5'>
-                                        <div className='label5'> HTML</div>
-                                  </div>
-                                  <img className='html51Icon'alt="" src={html} />
-                                  <div className='listItemPrimary6'>
-                                        <div className='documentIconLabel'>
-                                              <div className='label5'> CSS</div>
-                                        </div>
-                                  </div>
-                                  <img className='fileTypeCss2Icon' alt="" src={css} />
-                                  <div className='listItemPrimary7'>
-                                        <div className='documentIconLabel'>
-                                              <div className='label5'> 3D Modeling</div>
-                                        </div>
-                                  </div>
-                                  <img className='dModel1Icon' alt="threed" src={threed} />
-                                  <div className='listItemPrimary8'>
-                                        <div className='label5'> Java</div>
-                                  </div>
-                                  <img className='javaOriginal1Icon' alt="" src={java} />
-                                  <div className='listItemPrimary9'>
-                                        <div className='documentIconLabel'>
-                                              <div className='label5'> Next.js</div>
-                                        </div>
-                                  </div>
-                                  <div className='listItemPrimary10'>
-                                        <div className='label5'> Postgres</div>
-                                  </div>
-                                  <div className='listItemPrimary11'>
-                                        <div className='documentIconLabel'>
-                                              <div className='label5'> AWS</div>
-                                        </div>
-                                  </div>
-                                  <div className='listItemPrimary12'>
-                                        <div className='label5'> MySQL</div>
-                                  </div>
-                                  <img className='nextjs1Icon' alt="" src={nextjs} />
-                                  <img className='postgresql1Icon' alt="" src={postgresql} />
-                                  <img className='aws1Icon' alt="" src={aws} />
-                                  <img className='mysqlOriginalWordmark1Icon' alt="" src={mysql} />
-                                  <img className='fileTypeSass1Icon' alt="" src={sass} />
-                                  <div className='header'>
+                            <div className='separator' />
+                            <div className='contentAreaAndToolbar'>
+                                <div className='contentArea'>
+                                    
+                                    <div className='margins' />
+                                    
+                                    <div className='header'>
                                         <div className='labelRightDetail'>
-                                              <div className='label18'>Name</div>
-                                              <div className='rightDetail' />
+                                            <div className='label18'>Name</div>
+                                            <div className='rightDetail' />
                                         </div>
-                                        <div className='separator1'/>
-                                  </div>
-                                  <div className='listItemPrimary13'>
-                                        <div className='documentIconLabel7'>
-                                              <div className='label5'> Javascript</div>
+                                        <div className='separator1' />
+
+                                        <div>
+                                          <img  src={jestelleweb1}/>
                                         </div>
-                                  </div>
-                                  <img className='javascriptJs1Icon' alt="" src={javascript} />
-                            </div>
-                            <div className='toolbar1'>
-                                  <div className='leading5'>
+                                    </div>
+                                </div>
+                                <div className='toolbar1'>
+                                    <div className='leading5'>
                                         <div className='titleSidebarAndNavigation'>
-                                              <div className='sidebarButton'>
-                                                    <div className='symbol7'>􀏚</div>
-                                              </div>
-                                              <div className='finder-title'>Skills</div>
+                                            <div className='sidebarButton'>
+                                                <div className='symbol7'>􀏚</div>
+                                            </div>
+                                            <div className='finder-title'>Projects</div>
                                         </div>
-                                  </div>
-                                  <div className='trailing5'>
-                                        <div className='spacer'/>
+                                    </div>
+                                    <div className='trailing5'>
+                                        <div className='spacer' />
                                         <div className='spacer' />
                                         <div className='spacer' />
                                         <div className='search'>
-                                              <div className='symbol8'>􀊫</div>
-                                              <div className='searchLabel'>Search</div>
+                                            <div className='symbol8'>􀊫</div>
+                                            <div className='searchLabel'>Search</div>
                                         </div>
-                                  </div>
+                                    </div>
+                                </div>
                             </div>
-                      </div>
-                </div>
-            </div>
-    
-    
-  
-         )}
-        </>        
-            
-    
-  );
-}
+                        </div>
+                    </div>
+                )}
+            </>
+        );
+    }
 }
 
-export default Projects
+export default Projects;
